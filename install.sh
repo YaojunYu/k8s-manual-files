bbf498347d48aeaa89057242c51620e6d1f27658
bingoyu <yaojunchina@qq.com>
2018-08-21 22:21:38 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 for NODE in k8s-m1 k8s-m2 k8s-m3; do
     ssh ${NODE} "systemctl stop docker && systemctl stop kubelet"
-    ssh ${NODE} "rm -rf /etc/kubernetes && rm -rf ~/.kube && rm -rf /etc/etcd && rm -rf /var/lib/kubelet && rm -rf /var/log/kubernetes && rm -rf /var/lib/etcd && rm -rf /etc/systemd/system/kubelet.service.d"
+    ssh ${NODE} "rm -rf /etc/kubernetes && rm -rf ~/.kube && rm -rf /etc/etcd && rm -rf /var/lib/kubelet && rm -rf /var/log/kubernetes && rm -rf /var/lib/etcd && rm -rf /etc/systemd/system/kubelet.service.d && rm -rf /usr/local/bin/kubelet && rm -rf /usr/local/bin/kubectl"
     echo ""
     echo "== stop & disable firewalld, set SELINUX disabled @${NODE} =="
     ssh ${NODE} "systemctl stop firewalld && systemctl disable firewalld && setenforce 0 && sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config"
