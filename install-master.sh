2c5be0378862ff28bb967a7133e3d3e99b9564da
bingoyu <yaojunchina@qq.com>
2018-08-23 00:25:02 +0800

    update ingress

--- install-master.sh
+++ install-master.sh
@@ -1,7 +1,7 @@
 for NODE in k8s-m1 k8s-m2 k8s-m3; do
     ssh ${NODE} "systemctl stop docker && systemctl stop kubelet"
-    ssh ${NODE} "umount $(df -HT | grep '/var/lib/kubelet/pods' | awk '{print $7}') && rm -rf /var/lib/kubelet"
-    ssh ${NODE} "rm -rf /etc/kubernetes && rm -rf ~/.kube && rm -rf /etc/etcd && rm -rf /var/log/kubernetes && rm -rf /var/lib/etcd && rm -rf /etc/systemd/system/kubelet.service.d && rm -rf /usr/local/bin/kubelet && rm -rf /usr/local/bin/kubectl"
+    ssh ${NODE} "umount $(df -HT | grep '/var/lib/kubelet/pods' | awk '{print $7}')"
+    ssh ${NODE} "rm -rf /var/lib/kubelet && rm -rf /etc/kubernetes && rm -rf ~/.kube && rm -rf /etc/etcd && rm -rf /var/log/kubernetes && rm -rf /var/lib/etcd && rm -rf /etc/systemd/system/kubelet.service.d && rm -rf /usr/local/bin/kubelet && rm -rf /usr/local/bin/kubectl"
     echo ""
     echo "== stop & disable firewalld, set SELINUX disabled @${NODE} =="
     ssh ${NODE} "systemctl stop firewalld && systemctl disable firewalld && setenforce 0 && sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config"
