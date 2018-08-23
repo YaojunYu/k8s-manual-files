6b1d570c79661f0a488ce5b1c30ae0a3c44b5e0e
bingoyu <yaojunchina@qq.com>
2018-08-23 18:59:33 +0800

    update clean dir

--- clean.sh
+++ clean.sh
@@ -2,7 +2,16 @@
     echo "=====${NODE}===="
     ssh ${NODE} "systemctl stop docker && systemctl disable docker && systemctl disable kubelet && systemctl stop kubelet"
     ssh ${NODE} "umount \$(df -HT | grep '/var/lib/kubelet/pods' | awk '{print \$7}')"
-    ssh ${NODE} "rm -rf /etc/kubernetes && rm -rf ~/.kube && rm -rf /etc/etcd && rm -rf /var/lib/kubelet && rm -rf /var/log/kubernetes && rm -rf /var/lib/etcd && rm -rf /etc/systemd/system/kubelet.service.d && rm -rf /usr/local/bin/kubelet && rm -rf /usr/local/sbin/kubectl"
+    ssh ${NODE} "rm -rf /etc/kubernetes && \
+    rm -rf ~/.kube && \
+    rm -rf /etc/etcd && \
+    rm -rf /var/lib/kubelet && \
+    rm -rf /var/log/kubernetes && \
+    rm -rf /var/lib/etcd && \
+    rm -rf /etc/systemd/system/kubelet.service.d && \
+    rm -rf /lib/systemd/system/kubelet.service && \
+    rm -rf /usr/local/bin/kubelet && \
+    rm -rf /usr/local/sbin/kubectl"
 done
 
   
